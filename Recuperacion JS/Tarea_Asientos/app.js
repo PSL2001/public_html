eb03e0c4b7c665c4b590eb7f0fe99222338b9ba1
PSL2001 <pablitosanchezlopez@gmail.com>
2021-04-20 13:58:45 +0200

    Un apaño

--- Recuperacion JS/Tarea_Asientos/app.js
+++ Recuperacion JS/Tarea_Asientos/app.js
@@ -1,8 +1,12 @@
 window.onload = iniciar;
 
+const SELECTED = "seleccionado";
 const pelicula = document.getElementById("pelicula");
+const contenedor = document.getElementsByClassName("contenedor")[0];
+
 
 function iniciar() {
+    lectura();
     document.getElementsByClassName("contenedor")[0].addEventListener("click", dondeClick);
     pelicula.addEventListener("change", calcularPrecio);
 }
@@ -12,10 +16,12 @@ function dondeClick(event) {
         event.target.classList.add("seleccionado");
         document.getElementById("numero").innerHTML++;
         calcularPrecio();
+        almacenarAsientos()
     } else if (event.target.className == "asiento seleccionado") {
         event.target.classList.remove("seleccionado");
         document.getElementById("numero").innerHTML--;
         calcularPrecio();
+        almacenarAsientos();
     }
 }
 
@@ -27,4 +33,37 @@ function calcularPrecio() {
     precioTotal = cantidadAsientos * seleccionado;
     document.getElementById("total").innerHTML = precioTotal;
 
+    //Guardo en storage el precio y los asientos seleccionados
+    localStorage.setItem("cantidadAsientos", cantidadAsientos);
+    localStorage.setItem("precioTotal", precioTotal);
+    localStorage.setItem("pelicula", seleccionado);
+}
+
+function almacenarAsientos() {
+    let asientos = contenedor.querySelectorAll(".asiento")
+    let asientoseleccionado = [];
+    for (let i = 0; i < asientos.length; i++) {
+        if (asientos[i].classList.contains("seleccionado")) {
+            asientoseleccionado.push(i);
+        }
+    }
+    localStorage.setItem(SELECTED, JSON.stringify(asientoseleccionado));
+}
+
+function lectura() {
+    //Asientos
+    let asientoseleccionado = JSON.parse(localStorage.getItem(SELECTED));
+    let asientos = contenedor.querySelectorAll(".asiento");
+
+    for (let i = 0; i < asientoseleccionado.length; i++) {
+        asientos[asientoseleccionado[i]].classList.add("seleccionado");
+    }
+
+    //pelicula
+
+    //Precio total y cantidad de asientos
+    let precio = localStorage.getItem("preciototal");
+    document.getElementById("total").innerHTML = precio;
+    let cantidad = localStorage.getItem("cantidadAsientos");
+    document.getElementById("numero").innerHTML = cantidad;
 }
