abeab58fb0a01986a471f9f475520d545fd10d27
PSL2001 <65461723+PSL2001@users.noreply.github.com>
2021-05-08 00:26:53 +0200

    Esta ahi casi, pero ahi

--- Recuperacion JS/CODIGO_FUENTE_MARZO(1)/E2/js/app2.js
+++ Recuperacion JS/CODIGO_FUENTE_MARZO(1)/E2/js/app2.js
@@ -1,16 +1,36 @@
-window.onload = iniciar;
+window.onload=iniciar;
 
-let contenedorBTN;
+const buttons=document.querySelector(".sortBtn");
 
-function iniciar() {
-    contenedorBTN = document.querySelector(".sortBtn");
-    contenedorBTN.addEventListener("click", filtrar);
+function iniciar(){
+    buttons.addEventListener("click",filtrar);
 }
 
-function filtrar(event) {
-    if (event.target.tagName.toLowerCase() === "a") {
-        console.log("estoy cogiendo el boton");
-    } else {
-        console.log("no estoy cogiendo el boton");
-    }
-}
+//Hemos añadido en el archivo all.css una clase llamada .hidden{display:none;}
+function filtrar(e){
+    //Si el elemento clickeado es un <a></a>
+    if(e.target.tagName.toLowerCase() === "a"){
+        //Cogemos la lista entera de pasteles y sus divs
+        const lista=document.querySelectorAll('.store-items div');
+        //Cogemos el dato que estamos filtrando
+        let filtrado=e.target.dataset.filter;
+
+        //Si el filtro es all removemos la clase hidden
+        if(filtrado==="all"){
+            lista.forEach(pastel => pastel.classList.remove('hidden'));
+        }else{
+            //Pasamos por la lista de pasteles
+            lista.forEach(pastel => {
+                if(pastel.classList.contains(filtrado)){
+                    //Si el pastel contiene la clase que queremos filtrar
+                    //removemos la clase hidden
+                    pastel.classList.remove('hidden');
+                }else{
+                    //Si no contiene la clase filtrada añadimos la clase hidden
+                    pastel.classList.add('hidden');
+                }
+            });
+        }
+    }//Si no es un elemento <a></a> no hacemos nada
+
+}//FINFiltrado
