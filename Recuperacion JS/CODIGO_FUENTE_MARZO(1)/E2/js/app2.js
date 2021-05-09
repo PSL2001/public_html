4cf7335c6a6ac27f446da84d042b88a8b336a899
PSL2001 <65461723+PSL2001@users.noreply.github.com>
2021-05-09 22:40:42 +0200

    Cosorro

--- Recuperacion JS/CODIGO_FUENTE_MARZO(1)/E2/js/app2.js
+++ Recuperacion JS/CODIGO_FUENTE_MARZO(1)/E2/js/app2.js
@@ -1,33 +1,33 @@
-window.onload=iniciar;
+window.onload = iniciar;
 
-const buttons=document.querySelector(".sortBtn");
+const buttons = document.querySelector(".sortBtn");
 
-function iniciar(){
-    buttons.addEventListener("click",filtrar);
+function iniciar() {
+    buttons.addEventListener("click", filtrar);
 }
 
 //Hemos añadido en el archivo all.css una clase llamada .hidden{display:none;}
-function filtrar(e){
+function filtrar(e) {
     //Si el elemento clickeado es un <a></a>
-    if(e.target.tagName.toLowerCase() === "a"){
+    if (e.target.tagName.toLowerCase() === "a") {
         //Cogemos la lista entera de pasteles y sus divs
-        const lista=document.querySelectorAll('.store-items div');
+        const lista = document.querySelectorAll('.store-items div');
         //Cogemos el dato que estamos filtrando
-        let filtrado=e.target.dataset.filter;
+        let filtro = e.target.dataset.filter;
 
         //Si el filtro es all removemos la clase hidden
-        if(filtrado==="all"){
-            lista.forEach(pastel => pastel.classList.remove('hidden'));
-        }else{
+        if (filtro === "all") {
+            lista.forEach(producto => producto.classList.remove('hidden'));
+        } else {
             //Pasamos por la lista de pasteles
-            lista.forEach(pastel => {
-                if(pastel.classList.contains(filtrado)){
-                    //Si el pastel contiene la clase que queremos filtrar
+            lista.forEach(producto => {
+                if (producto.classList.contains(filtro)) {
+                    //Si el producto contiene la clase que queremos filtrar
                     //removemos la clase hidden
-                    pastel.classList.remove('hidden');
-                }else{
+                    producto.classList.remove('hidden');
+                } else {
                     //Si no contiene la clase filtrada añadimos la clase hidden
-                    pastel.classList.add('hidden');
+                    producto.classList.add('hidden');
                 }
             });
         }
