6f8c0625aa667bc654ef23df2c2c825548ee5a3c
PSL2001 <65461723+PSL2001@users.noreply.github.com>
2021-05-09 22:42:13 +0200

    Ya esta, se acabo la pesadilla

--- Recuperacion JS/CODIGO_FUENTE_MARZO(1)/E2/js/app2.js
+++ Recuperacion JS/CODIGO_FUENTE_MARZO(1)/E2/js/app2.js
@@ -1,35 +1,37 @@
-window.onload = iniciar;
+window.onload=iniciar;
 
-const buttons = document.querySelector(".sortBtn");
+const buttons=document.querySelector(".sortBtn");
 
-function iniciar() {
-    buttons.addEventListener("click", filtrar);
+function iniciar(){
+    buttons.addEventListener("click",filtrar);
 }
 
 //Hemos añadido en el archivo all.css una clase llamada .hidden{display:none;}
-function filtrar(e) {
+function filtrar(e){
     //Si el elemento clickeado es un <a></a>
-    if (e.target.tagName.toLowerCase() === "a") {
+    if(e.target.tagName.toLowerCase() === "a"){
         //Cogemos la lista entera de pasteles y sus divs
-        const lista = document.querySelectorAll('.store-items div');
+        const lista=document.querySelectorAll('.store-item');
         //Cogemos el dato que estamos filtrando
-        let filtro = e.target.dataset.filter;
+        let filtrado=e.target.dataset.filter;
 
         //Si el filtro es all removemos la clase hidden
-        if (filtro === "all") {
-            lista.forEach(producto => producto.classList.remove('hidden'));
-        } else {
-            //Pasamos por la lista de pasteles
-            lista.forEach(producto => {
-                if (producto.classList.contains(filtro)) {
-                    //Si el producto contiene la clase que queremos filtrar
-                    //removemos la clase hidden
-                    producto.classList.remove('hidden');
-                } else {
-                    //Si no contiene la clase filtrada añadimos la clase hidden
-                    producto.classList.add('hidden');
-                }
+        if(filtrado==="all"){
+            lista.forEach(pastel => {
+                if(pastel.classList.contains('hidden'))
+                    pastel.classList.remove('hidden');
             });
+        }else{
+            for(let i=0;i<lista.length;i++){
+                if(lista[i].dataset.item==filtrado){
+                    //Si el pastel contiene la clase que queremos filtrar
+                    //removemos la clase hidden
+                    lista[i].classList.remove('hidden');
+                }else{
+                    //Si no contiene la clase filtrada añadimos la clase hidden
+                    lista[i].classList.add('hidden');
+                }
+            }
         }
     }//Si no es un elemento <a></a> no hacemos nada
 
